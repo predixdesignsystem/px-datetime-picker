bb06209c4e636f6bfd5085fdfdb5826fcd7d59a8
katemenkhaus <katherine.menkhaus@ge.com>
2017-10-31 10:34:40 -0700

    updated tests

--- test/px-datetime-picker-tests.js
+++ test/px-datetime-picker-tests.js
@@ -1,8 +1,3 @@
-document.addEventListener("WebComponentsReady", function() {
-  runCustomTests();
-});
-
-function runCustomTests() {
 
   describe('px-datetime-picker', function() {
 
@@ -12,52 +7,56 @@ function runCustomTests() {
       expect(pickerEl._opened).to.be.false;
     });
 
-    // it('the calendar opens when date icon is clicked', function(done){
-    //   var pickerEl = fixture('DatetimePicker');
+    it('the calendar opens when date icon is clicked', function(done){
+      var pickerEl = fixture('DatetimePicker');
 
-    //   //click on the date icon
-    //   flush (function(){
-    //     dateEl = pickerEl.querySelector('#date');
-    //     dateIconEl = pickerEl.querySelector('px-icon');
-    //     dateIconEl.click();
-    //     done();
-    //   });
+      //click on the date icon
+      flush (function(){
+        dateEl = pickerEl.querySelector('#date');
+        dateIconEl = pickerEl.querySelector('px-icon');
+        dateIconEl.click();
+        done();
+      });
 
-    //   flush (function() {
-    //     expect(pickerEl._opened).to.be.true;
-    //   });
-    // });
+      flush (function() {
+        expect(pickerEl._opened).to.be.true;
+      });
+    });
 
-    // it('the calendar closes when clicking outside of panel', function(done){
-    //   var pickerEl = fixture('DatetimePicker');
+    it('the calendar closes when clicking outside of panel', function(done){
+      var pickerEl = fixture('DatetimePicker');
 
-    //   //click on the date icon
-    //   flush (function(){
-    //     dateEl = pickerEl.querySelector('#date');
-    //     dateIconEl = pickerEl.querySelector('px-icon');
-    //     dateIconEl.click();
-    //   });
+      //click on the date icon
+      flush (function(){
+        dateEl = pickerEl.querySelector('#date');
+        dateIconEl = pickerEl.querySelector('px-icon');
+        dateIconEl.click();
+      });
 
-    //   flush (function() {
-    //     expect(pickerEl._opened).to.be.true;
-    //   });
+      flush (function() {
+        expect(pickerEl._opened).to.be.true;
+      });
 
-    //   flush (function() {
-    //     document.body.click();
-    //     expect(pickerEl._opened).to.be.false;
-    //     done();
-    //   });
-    // });
+      flush (function() {
+        document.body.click();
+      });
 
-    // it('check that _opened draws calendar opens on method _open', function(done){
-    //   var pickerEl = fixture('DatetimePicker');
+      flush (function() {
+        debugger
+        expect(pickerEl._opened).to.be.false;
+        done();
+      });
+    });
 
-    //   //click on the date icon
-    //   flush (function(){
-    //     expect(pickerEl._opened).to.be.false;
-    //     done();
-    //   });
-    // });
+    it('check that _opened draws calendar opens on method _open', function(done){
+      var pickerEl = fixture('DatetimePicker');
+
+      //click on the date icon
+      flush (function(){
+        expect(pickerEl._opened).to.be.false;
+        done();
+      });
+    });
 
 
 
@@ -69,4 +68,3 @@ function runCustomTests() {
 
 
   });//end of describe
-}
